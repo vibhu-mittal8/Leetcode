d26fdd974866b40c852cc5778552fd27e1ac71b4
VIBHU MITTAL <vibhumittal8@gmail.com>
2026-09-25 12:21:51 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (36.10%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n<=2) return n;
+        int a=1;
+        int b=2;
+        for(int i=3;i<=n;i++){
+            int curr=a+b;
+            a=b;
+            b=curr;
+
+        }
+        return b;
+    }
+}
